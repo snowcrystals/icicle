082465851f15dcc44f3518fcff3db8a8bee59d1c
Daan Klarenbeek <github@ijskoud.dev>
2022-11-25 19:42:26 +0000

    feat: add logger (#4)
    * chore: hello world
    
    * fix: package not installable due to postinstall script
    
    * ci: add npm publish on release workflow
    
    * feat: add logger code
    
    * chore(Deps): add missing dependency
    
    * chore: add esm & cjs building via tsup
    
    * chore: add npm publish instructions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello World!");
+export * from "./lib/Logger.js";
+export * from "./lib/LoggerLevel.js";
+export * from "./lib/LoggerStyle.js";
+export * from "./lib/LoggerTimestamp.js";
+export * from "./lib/LoggerTypes.js";
